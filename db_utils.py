8a8481048437aa9e7e41dfa44bae45a00f6cade4
balany1 <andrewmcnamara@live.co.uk>
2024-08-16 13:54:53 +0100

    new classes added

--- db_utils.py
+++ db_utils.py
@@ -74,22 +74,36 @@ class DataTransform:
       df['next_payment_date'] = pd.to_datetime(df['next_payment_date'],dayfirst=False, format="mixed")
       df['last_credit_pull_date'] = pd.to_datetime(df['last_credit_pull_date'],dayfirst=False, format="mixed")
       
+      #print(df.dtypes)
       #print(df)
       #return df
 
+class DataFrameInfo:
+   
    def describe_dataframe(self, file_name):
 
       df_stats = self.set_data_frame(file_name)
       print(df_stats)
 
    def df_shape(self, file_name):
+      #prints the shape of the dataframe
       df = self.set_data_frame(file_name)
       print(df.shape)
 
    def store_stats(self,file_name):
-      pass
+      #computes statistics for numerical columns
+
+      df = self.set_data_frame(file_name)
+      # for column in df:
+      #    if df[column].dtype == 'int64' or df[column].dtype == 'float64':
+      #       quant_cols.append(column)
+      
+      stats = df.describe(include=np.number).applymap(lambda x: f"{x:0.2f}")
+      print(stats)
 
    def count_distinct(self,file_name):
+      #finds the number of unique entries for columns with categorical data
+
       df = self.set_data_frame(file_name)
 
       app_type_counts = df['application_type'].value_counts()
@@ -97,44 +111,60 @@ class DataTransform:
       ver_status_counts = df['verification_status'].value_counts()
       home_own_counts = df['home_ownership'].value_counts()
       purpose_counts = df['home_ownership'].value_counts()
+      grade_counts = df['grade'].value_counts()
+      subgrade_counts = df['subgrade'].value_counts()
 
       print("unique application types: " + str(app_type_counts.count()))
       print("unique loan statuses: " + str(loan_status_counts.count()))
       print("unique verification statuses: " + str(ver_status_counts.count()))
       print("unique home ownership statuses: " + str(ver_status_counts.count()))
       print("unique purposes : " + str(ver_status_counts.count()))
+      print("unique grades : " + str(ver_status_counts.count()))
+      print("unique subgrades : " + str(ver_status_counts.count()))
 
    def null_values(self,file_name):
-      pass
-
-   def display_percentiles(self,file_name):
-      pass
-
-   def averages_sd_var(self,file_name):
-      pass
+      df = self.set_data_frame(file_name)
+      null_values = df.isna().sum() 
+      pcnt_null = null_values/df.shape[0] * 100
+      print(pcnt_null)
 
 
-
-
-
-#       Describe all columns in the DataFrame to check their data types
-# Extract statistical values: median, standard deviation and mean from the columns and the DataFrame
-# Count distinct values in categorical columns
-# Print out the shape of the DataFrame
-# Generate a count/percentage count of NULL values in each column
-# Any other methods you may find useful
-
+class DataFrameTransform:
       
+      def set_data_frame(self, file_name):
+      
+         #read_csv_file
+         df = pd.read_csv(file_name)
+         #print(df.dtypes)
+         return df
+
+      def null_values(self,file_name):
+         df = self.set_data_frame(file_name)
+         null_values = df.isna().sum() 
+         pcnt_null = null_values/df.shape[0] * 100
+         print(pcnt_null)
+
+
+      def Nullremoval(self):
+         pass
+         
+
+class Plotter:
+      pass
+# Any other methods you may find useful
 
 
 if __name__ == "__main__":
 
    #data = RDSDatabaseConnector()
    dataframe = DataTransform()
-   df = dataframe.fix_data_types('loan_payments.csv')
-   dataframe.describe_dataframe('loan_payments.csv')
-   dataframe.count_distinct('loan_payments.csv')
-   dataframe.df_shape('loan_payments.csv')
+   #df = dataframe.fix_data_types('loan_payments.csv')
+   #dataframe.describe_dataframe('loan_payments.csv')
+   #dataframe.count_distinct('loan_payments.csv')
+   #dataframe.df_shape('loan_payments.csv')
+   #dataframe.store_stats('loan_payments.csv')
+   dataframe.null_values('loan_payments.csv')
+   
    
    
 
